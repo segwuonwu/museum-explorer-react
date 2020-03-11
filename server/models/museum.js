c0dc981f3c50f513466dfcbc73e927549ab8ce66
009kings <009kings@gmail.com>
2020-03-11 00:18:25 -0700

    Made them models

--- server/models/museum.js
+++ server/models/museum.js
@@ -1,8 +1,28 @@
 // Require Mongoose node module
 const mongoose = require('mongoose');
 
-// TODO: Create Museum Schema
+// TODO: Create Museum Schema name, country, city, image
+const museumSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    default: 'http://www.placekitten.com/400/600'
+  }
+})
 
 // TODO: Use schema to create model
+const Museum = mongoose.model('Museum', museumSchema);
 
 // TODO: Export Museum Model
+module.exports = Museum;
--- server/models/piece.js
+++ server/models/piece.js
@@ -1,16 +1,39 @@
 // Require Mongoose node module
 const mongoose = require('mongoose');
 
-// TODO: Create Creator Schema
+// TODO: Create Creator Schema, name, image, birth year, death year
+const creatorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    default: 'Anonymous'
+  },
+  image: {
+    type: String,
+    default: 'http://www.placekitten.com/200/200'
+  },
+  birthYear: String,
+  deathYear: String
+});
 
-// TODO: Create Piece Schema
-// HINT: include a creator field for using the Creator schema
+// TODO: Create Piece Schema name, image, creator, museum
+const pieceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    default: 'Untitled'
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  creator: creatorSchema,
+  museum: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Museum'
+  }
+});
 
 // TODO: Use Piece schema to create Piece model
+const Piece = mongoose.model('Piece', pieceSchema);
 
 // TODO: Export Piece Model
-
-
-// NOTE: You don't need to worry about Creator schema. You don't need to
-// create a model for it or export it. This is because it lives inside
-// the Piece model, so that takes care of it all! Yay for embedded schemas!
+module.exports = Piece;
