2234d528309331f8d73a0ce0b6ab05bcca0784bc
009kings <009kings@gmail.com>
2020-03-10 22:10:24 -0700

    Check out that basic decoupled structure

---
+++ server/models/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// Connect to Mongo database
+mongoose.connect(
+  process.env.MONGO_URL || 'mongodb://localhost:27017/museumexplorer',
+  { useNewUrlParser: true }
+);
+let db = mongoose.connection;
+db.once('open', () => {
+  console.log(`🔗 Connected to MongoDB on ${db.host}: ${db.port}`);
+});
+db.on('error', err => {
+  console.log(`🐻 Bad news bears! MongoDB error:\n${err}`);
+});
+
+// Require your other models, and export them
+// NOTE: Your files can have multiple module.exports statements!
+// Make sure to export both your Museum and Piece models!
+module.exports.Museum = require('./museum');
+module.exports.Piece = require('./piece');
---
+++ server/server.js
@@ -0,0 +1,22 @@
+// Require needed node modules
+const express = require('express');
+
+// Create an instance of express
+const app = express();
+
+// Middleware, etc
+app.use(express.urlencoded({ extended: false }));
+
+// Declare controllers
+app.use('/museums', require('./routes/museums'));
+app.use('/pieces', require('./routes/pieces'));
+
+// Make 404 route
+app.get('/', (req, res) => {
+  res.json('error', 'page not found');
+});
+
+// Listen
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`🎧 You're listening to the smooth sounds of port ${process.env.PORT || 3000} ☕️`)
+});
