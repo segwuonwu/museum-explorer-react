bc9094bd138d77ab5524d5cdc48edf119289cf57
009kings <009kings@gmail.com>
2020-03-11 00:34:20 -0700

    museum routes up

--- server/routes/museums.js
+++ server/routes/museums.js
@@ -1,5 +1,22 @@
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
+const db = require('../models/index');
 
+router.get('/', (req, res) => {
+  db.Museum.find()
+  .then(museums => res.send(museums))
+  .catch(err=>res.send({ message: 'Error in getting all museums', err}));
+});
+
+router.post('/', (req, res) => {
+  db.Museum.create(req.body)
+  .then(museum => res.send(museum))
+})
+
+router.get('/:id', (req, res) => {
+  db.Museum.findById(req.params.id)
+  .then(museum => res.send(museum))
+  .catch(err=>res.send({ message: 'Error in getting one museums', err}));
+});
 
 module.exports = router;
